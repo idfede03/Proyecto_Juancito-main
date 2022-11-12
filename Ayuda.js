0727a97bfdeadb71fba5272225b4efa163d8d1ff
feder <federicojuarez2002@htomail.com>
2022-11-12 11:54:12 -0300

    12 de Noviembre

---
+++ Ayuda.js
@@ -0,0 +1,8 @@
+$(Document).Ready (function(){
+    swal({
+        title: "Buen Trabajo!",
+        text: "Su comentario fue envia correctamente!",
+        icon: "success",
+        button: "Aww yiss!",
+      });
+})
